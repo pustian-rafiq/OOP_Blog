3f76513c844939449560c64c7f616fa333f504df
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-06-03 22:05:24 +0600

    mastering modify admin dashboard

---
+++ admin/inc/footer.php
@@ -0,0 +1,70 @@
+
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>document.write(new Date().getFullYear())</script> © Minible.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Crafted with <i class="mdi mdi-heart text-danger"></i> by <a href="https://themesbrand.com/" target="_blank" class="text-reset">Themesbrand</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+             
+             </div>
+             <!-- end main content-->
+ 
+         </div>
+         <!-- END layout-wrapper -->
+ 
+         <!-- Right Sidebar -->
+         <?php require_once "inc/setting.php" ?>
+         <!-- /Right-bar -->
+ 
+         <!-- Right bar overlay-->
+         <div class="rightbar-overlay"></div>
+ 
+         <!-- JAVASCRIPT -->
+         <script src="assets/libs/jquery/jquery.min.js"></script>
+         <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+         <script src="assets/libs/metismenu/metisMenu.min.js"></script>
+         <script src="assets/libs/simplebar/simplebar.min.js"></script>
+         <script src="assets/libs/node-waves/waves.min.js"></script>
+         <script src="assets/libs/waypoints/lib/jquery.waypoints.min.js"></script>
+         <script src="assets/libs/jquery.counterup/jquery.counterup.min.js"></script>
+ 
+         <!-- apexcharts -->
+         <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+ 
+         <script src="assets/js/pages/dashboard.init.js"></script>
+ 
+         <!-- App js -->
+         <script src="assets/js/app.js"></script>
+ 
+     </body>
+ 
+ 
+ <!-- Mirrored from themesbrand.com/minible/layouts/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 15 Jul 2021 13:44:57 GMT -->
+ </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- admin/inc/header.php
+++ admin/inc/header.php
@@ -1,4 +1,34 @@
-      
+<!doctype html>
+<html lang="en">
+
+    
+<!-- Mirrored from themesbrand.com/minible/layouts/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 15 Jul 2021 13:44:25 GMT -->
+<head>
+
+        <meta charset="utf-8" />
+        <title>Dashboard | Minible - Admin & Dashboard Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesbrand" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    
+    <body>
+
+    <!-- <body data-layout="horizontal" data-topbar="colored"> -->
+
+        <!-- Begin page -->
+        <div id="layout-wrapper">
             <header id="page-topbar">
                 <div class="navbar-header">
                     <div class="d-flex">
@@ -261,7 +291,7 @@
 
                         <div class="dropdown d-inline-block">
                             <button type="button" class="btn header-item noti-icon right-bar-toggle waves-effect">
-                                <i class="uil-cog"></i>
+                                <i class="fas fa-cog"></i>
                             </button>
                         </div>
             
